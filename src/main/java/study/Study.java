651e48f2ce518b26fb38706954764ba47a3905ac
Masaharu Taketani@DNITS <masaharu_taketani@dnitsol.com>
2019-02-26 11:19:16 +0900

    Java7との比較を追加

--- src/main/java/study/Study.java
+++ src/main/java/study/Study.java
@@ -1,29 +1,52 @@
 package study;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Study {
+
+    private static final List<Integer> NUMBERS = Arrays.asList(new Integer[]{1, 12, 3, 3, 4, 6, 2});
 
     /**
      * main関数
      */
     public static void main(String[] args) {
 
-        study();
+        System.out.println("Java8の結果");
+        onlyEvenAndSortByJava8().forEach(System.out::println);
+
+        System.out.println("Java7の結果");
+        onlyEvenAndSortByJava7().forEach(System.out::println);
     }
 
     /**
-     * 演習用のメソッド
+     * 演習用のメソッド(java8)
      */
-    public static void study() {
-
-        List<Integer> numbers = Arrays.asList(new Integer[]{1, 12, 3, 3, 4, 6, 2});
+    public static List<Integer> onlyEvenAndSortByJava8() {
 
         //与えた整数から、偶数だけを抜き出し、昇順に並び替え、標準出力
-        numbers.stream()
-            .filter(each -> each % 2 == 0)
-            .sorted()
-            .forEach(s -> System.out.println(s));
+        return NUMBERS.stream()
+                    .filter(each -> each % 2 == 0)
+                    .sorted().collect(Collectors.toList());
+    }
+
+
+    /**
+     * 演習用のメソッド(java7)
+     */
+    public static List<Integer> onlyEvenAndSortByJava7() {
+
+        List<Integer> even = new ArrayList<Integer>();
+        for (Integer num : NUMBERS) {
+            if (num % 2 == 0) {
+                even.add(num);
+            }
+        }
+        Collections.sort(even);
+
+        return even;
     }
 }
